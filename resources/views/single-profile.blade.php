6cf706998184116e77c21aebad81b47f10589391
Rizqy H <rizqy@dicoding.com>
2024-03-07 21:16:39 +0700

    add simple profile nav

--- resources/views/single-profile.blade.php
+++ resources/views/single-profile.blade.php
@@ -10,6 +10,11 @@
                 <div>{{ $user->posts->count() }} posts</div>
             </div>
         </div>
+
+        <div class="grid grid-cols-2 mt-6 -mb-6">
+            <a href="{{ route('users.profile', $user->username) }}" @class(['py-2 text-center', 'font-bold border-b-4 border-emerald-300' => request()->routeIs('users.profile')])>Posts</a>
+            <a href="{{ route('users.profile', $user->username) }}" @class(['py-2 text-center', 'font-bold border-b-4 border-emerald-300' => request()->routeIs('users.profil')])>Liked</a>
+        </div>
     </x-slot>
 
     <div class="space-y-4">
