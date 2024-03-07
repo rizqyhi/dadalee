afc6dfeb889c17897e2efbc65ce4ea37a5337fa4
Rizqy H <rizqy@dicoding.com>
2024-03-07 21:24:16 +0700

    restore routes

--- routes/auth.php
+++ routes/auth.php
@@ -36,23 +36,23 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::middleware('auth')->group(function () {
-//    Route::get('verify-email', EmailVerificationPromptController::class)
-//                ->name('verification.notice');
-//
-//    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-//                ->middleware(['signed', 'throttle:6,1'])
-//                ->name('verification.verify');
-//
-//    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-//                ->middleware('throttle:6,1')
-//                ->name('verification.send');
+    Route::get('verify-email', EmailVerificationPromptController::class)
+                ->name('verification.notice');
 
-//    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-//                ->name('password.confirm');
-//
-//    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
-//
-//    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+                ->middleware(['signed', 'throttle:6,1'])
+                ->name('verification.verify');
+
+    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+                ->middleware('throttle:6,1')
+                ->name('verification.send');
+
+    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+                ->name('password.confirm');
+
+    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+
+    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
